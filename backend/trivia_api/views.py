e7b465d387c5793ce6e4e02da7ed2c56eccabab1
f-hash <abimbolaakosile922@gmail.com>
2022-04-07 21:12:08 -0400

    Changed the login/signup methods by rewriting the database logic and by making rewriting the signup logic to only accept acceptable JSON requests, and further deteled the loaduser methods and changed teh logout method, In addition, fixing the merge conflicts between login/signup and main

--- backend/trivia_api/views.py
+++ backend/trivia_api/views.py
@@ -61,6 +61,48 @@ def users(user_id):
     return dummy_users
 
 
+@blueprint.route("/signup", methods=["POST"])
+def signup_post():
+    # return jsonify({"message": "success"}, 200)
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    newuser = User(username=username, password=password)
+
+    if User.query.filter_by(username=newuser.username).first() is None:
+
+        new_users = User(
+            username=username,
+            password=generate_password_hash(password, method="sha256"),
+        )
+
+        db.session.add(new_users)
+        db.session.commit()
+        return {"success": True}
+    else:
+        return {"success": False, "error": "username already taken"}
+
+
+@blueprint.route("/login", methods=["POST"])
+def login_post():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    #     remember = bool(request.form.get("remember"))
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password, password):
+        return {"success": False, "error": "invalid login"}
+    login_user(user)
+    return {"success": True}
+
+
+@blueprint.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return {"success": True}
+
+
 @blueprint.route("/api/quiz", methods=["POST"])
 def submit_quiz():
     data = request.get_json()
@@ -147,86 +189,3 @@ def get_leaderboard():
 @login_manager.unauthorized_handler
 def login_error():
     return {"status": "success", "error": "login required"}
-
-
-# get leaderboard/per category
-
-# @blueprint.route("/login")
-# def login():
-#     """
-#     Login flow entrypoint. All attempts to access authentication-gated pages
-#     will redirect here.
-#     """
-
-#     return render_template("login.html")
-
-
-# @blueprint.route("/login", methods=["POST"])
-# def login_post():
-#     """
-#     Login endpoint for authentication. Invalid login data will result in a
-#     message being flashed to the user. On authentication success, the user
-#     will be redirected to the randomizer page.
-#     """
-
-#     username = request.form.get("username")
-#     password = request.form.get("password")
-#     remember = bool(request.form.get("remember"))
-
-#     user = User.query.filter_by(username=username).first()
-
-#     if not user or not check_password_hash(user.password, password):
-#         flash("Please check your login details and try again.")
-#         return redirect(url_for("blueprint.login"))
-
-#     login_user(user, remember=remember)
-#     return redirect(url_for("blueprint.movies"))
-
-
-# @blueprint.route("/signup")
-# def signup():
-#     """
-#     Signup flow entrypoint. Accessible from the navbar when user is not
-#     authenticated.
-#     """
-
-#     return render_template("signup.html")
-
-
-# @blueprint.route("/signup", methods=["POST"])
-# def signup_post():
-#     """
-#     Signup endpoint. Ensures duplication of usernames is not possible,
-#     and hashes passwords for secure storage of user data.
-#     """
-
-#     username = request.form.get("username")
-#     password = request.form.get("password")
-
-#     user = User.query.filter_by(username=username).first()
-
-#     if user:
-#         flash("Username already exists")
-#         return redirect(url_for("blueprint.signup"))
-
-#     new_user = User(
-#         username=username,
-#         password=generate_password_hash(password, method="sha256"),
-#     )
-
-#     db.session.add(new_user)
-#     db.session.commit()
-
-#     return redirect(url_for("blueprint.login"))
-
-
-# @blueprint.route("/logout")
-# @login_required
-# def logout():
-#     """
-#     Logout endpoint. Deactivates user session and redirects user to
-#     login page.
-#     """
-
-#     logout_user()
-#     return redirect(url_for("blueprint.login"))
