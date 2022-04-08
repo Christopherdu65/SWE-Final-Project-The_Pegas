e2790207b267a1999262858ad9f8d6c3180dcbba
Scott Ross <scott.william.ross2@gmail.com>
2022-04-07 20:13:02 -0400

    finished avatars using dicebear avatars http api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,6 @@ import Profile from "./components/Profile";
 import Leaderboard from "./components/Leaderboard";
 
 function App() {
-    /*const [test, setTest] = useState({});
-
-    useEffect(() => {
-        fetch("/users/1", {
-            headers: {
-                accepts: "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setTest(data);
-            })
-            .catch(() => {
-                setTest("failure");
-            });
-    }, []);*/
 
     return (
             <div className="App">
--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,16 +1,11 @@
-import { createAvatar } from '@dicebear/avatars';
-import * as style from '@dicebear/avatars-human-sprites';
-
 const UserList = (props) => {
-    console.log(props);
     const users = props.users;
-    console.log(users);
 
     return (
         <div className="userlist">
            { users.map((user) => (
                 <div className="userlist-item">
-                    <img className="avatar" src={ createAvatar(style, { seed: user.username }) } alt="profile image"/>
+                    <img className="avatar" src={'https://avatars.dicebear.com/api/human/'  + user.username + '.svg'} alt="profile image"/>
                     <h2 className="username-list">{user.username}</h2>
                     <p className="score-list">{user.totalPoints}</p> 
                 </div>
