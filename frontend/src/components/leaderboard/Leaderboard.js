c3c72dad67d5fc5aa30f15dfbd4c39d45c6ec91a
Scott Ross <scott.william.ross2@gmail.com>
2022-04-08 18:05:15 -0400

    added dropdown menu using react-select, renamed UserList to LeaderList

--- frontend/src/components/leaderboard/Leaderboard.js
+++ frontend/src/components/leaderboard/Leaderboard.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
-import UserList from "./UserList"
+import { useState, useEffect, useImperativeHandle } from 'react';
+import LeaderList from "./LeaderList"
 import "./Leaderboard.css";
+import Select from 'react-select'
 
 function Leaderboard() {
 
-    const [leaders, setLeaders] = useState([]);
+    const [leaders, setLeaders] = useState();
+    const [category, setCategory] = useState();
 
     useEffect(() => {
         fetch('/api/leaderboard', {})
@@ -13,15 +15,33 @@ function Leaderboard() {
         .catch(error => console.log(error))
     }, []);
 
-    console.log(leaders)
+    console.log(leaders);
     // to-do: sort users by score beforer listing them out <UserList users={users}/>
+
+    const options = [
+        {value: 1, label: 'Cat1'},
+        {value: 2, label: 'Cat2'},
+        {value: 3, label: 'Cat3'}
+    ];
+
+    function handleOnChange(value) {
+        console.log(value)
+        setCategory(value.value)
+    }
 
     return (
         <div className="board">
             <h1>Leaderboard</h1>
                 
+            <center><Select 
+            className="selectcat"
+            options={options} 
+            placeholder="Pick a category!" 
+            onChange={handleOnChange}
+            /></center>
+
+            
             <div className="list">
-                
             </div>
         </div>    
 
