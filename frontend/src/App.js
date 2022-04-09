bd31b6398cd9d612cbbd86fad8aa7f60cb28b623
Christopherdu65 <dadadoytembe@gmail.com>
2022-04-08 22:48:00 -0400

    Added login and registration feature and api call to save user score after quiz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,53 +2,34 @@
 // import logo from "./logo.svg";
 // import { React, useEffect, useState } from "react";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Settings from "./components/quizComponent/Settings";
 import Quiz from "./components/quizComponent/Quiz";
+import Login from "./components/loginComponent/Login";
+import Register from "./components/registerComponent/Register";
 
 function App() {
-  // const [test, setTest] = useState({});
+  const [user, setUser] = useState(false);
 
-  // useEffect(() => {
-  //     fetch("/users/1", {
-  //         headers: {
-  //             accepts: "application/json",
-  //         },
-  //     })
-  //         .then((response) => response.json())
-  //         .then((data) => {
-  //             setTest(data);
-  //         })
-  //         .catch(() => {
-  //             setTest("failure");
-  //         });
-  // }, []);
-
-  // return (
-  //     <div className="App">
-  //         <header className="App-header">
-  //             <img src={logo} className="App-logo" alt="logo" />
-  //             <p>Username: {test["username"]}</p>
-  //             <a
-  //                 className="App-link"
-  //                 href="https://reactjs.org"
-  //                 target="_blank"
-  //                 rel="noopener noreferrer"
-  //             >
-  //                 Learn React
-  //             </a>
-  //         </header>
-  //     </div>
-  // );
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Settings />
+          {user ? <Settings setUser={setUser} /> : <Login setUser={setUser} />}
         </Route>
         <Route path="/quiz">
-          <Quiz />
+          {user ? <Quiz /> : <Login setUser={setUser} />}
+        </Route>
+        <Route path="/login">
+          {user ? <Settings setUser={setUser} /> : <Login setUser={setUser} />}
+        </Route>
+        <Route path="/register">
+          {user ? (
+            <Settings setUser={setUser} />
+          ) : (
+            <Register setUser={setUser} />
+          )}
         </Route>
       </Switch>
     </Router>
