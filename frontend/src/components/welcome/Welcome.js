b03b915d362bafdd4247de5328c34920aa28a0be
f-hash <abimbolaakosile922@gmail.com>
2022-04-14 13:10:40 -0400

    Designed the Navbar, created alerts for login/ signup and logout buttons, changed routes for the quiz and added new routes called about me,  Home, welcome and contact us

---
+++ frontend/src/components/welcome/Welcome.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
+// import logo from "./logo.svg";
+// import Logout from "./components/logout/Logout";
+// import { React, useEffect, useState } from "react";
+
+import React from 'react';
+
+function Welcome() {
+    return (
+        <div>
+            <div className="navigation">
+                <h2 className="title">Welcome to Trivia App. You can get started above</h2>
+            </div>
+
+
+        </div >
+    )
+
+}
+export default Welcome;
