4b6cda0bf5d8f546796c0b32c843614b38b4f66a
Rushi Patel <rnpnull@gmail.com>
2022-04-19 22:13:50 -0400

    Fixed leaderboard issue due to incorrect Promise handlers, fixed onEffect to properly fetch leaderboard data on category change, optimized category to ID mapping references by pulling to common file and exporting to components

--- frontend/src/components/leaderboard/Leaderboard.js
+++ frontend/src/components/leaderboard/Leaderboard.js
@@ -6,49 +6,46 @@ import { useState, useEffect } from "react";
 import Select from "react-select";
 import LeaderList from "./LeaderList";
 import "./Leaderboard.css";
+import categoryMap from "../common/categoryMap";
 
 function Leaderboard() {
-  const [leaders, setLeaders] = useState([]);
-  const [category, setCategory] = useState(0);
-  const [url, setUrl] = useState("/api/leaderboard");
+    const [leaders, setLeaders] = useState([]);
+    const [category, setCategory] = useState(0);
 
-  useEffect(() => {
-    fetch(url, {})
-      .then((response) => response.json())
-      .then((response) => setLeaders(response.results))
-      .then((response) => console.log(response.results))
-      .catch((error) => console.log(error));
-  }, []);
+    useEffect(() => {
+        fetch(`/api/leaderboard?category=${category}`, {})
+            .then((response) => response.json())
+            .then((response) => {
+                setLeaders(response.results);
+            })
+            .catch((error) => console.log(error));
+    }, [category]);
 
-  const options = [
-    { value: 0, label: "Cat0" },
-    { value: 1, label: "Cat1" },
-    { value: 20, label: "Cat20" },
-  ];
+    const options = Object.entries(categoryMap).map(([key, value]) => ({
+        value: key,
+        label: value,
+    }));
 
-  function handleOnChange(value) {
-    setCategory(value.value);
-    setUrl(`/api/leaderboard?category=${category}`);
-  }
+    return (
+        <div className="board">
+            <h1>Leaderboard</h1>
 
-  return (
-    <div className="board">
-      <h1>Leaderboard</h1>
+            <center>
+                <Select
+                    className="selectcat"
+                    options={options}
+                    placeholder="Pick a category!"
+                    onChange={(value) => {
+                        setCategory(value.value);
+                    }}
+                />
+            </center>
 
-      <center>
-        <Select
-          className="selectcat"
-          options={options}
-          placeholder="Pick a category!"
-          onChange={handleOnChange}
-        />
-      </center>
-
-      <div className="list">
-        <LeaderList leaders={leaders} />
-      </div>
-    </div>
-  );
+            <div className="list">
+                <LeaderList leaders={leaders} />
+            </div>
+        </div>
+    );
 }
 
 export default Leaderboard;
