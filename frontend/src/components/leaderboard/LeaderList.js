638fb62df9159339d4212fe04bdc61ddda7e7f3a
Christopherdu65 <dadadoytembe@gmail.com>
2022-04-20 19:07:34 -0400

    Fixed default number of questions, login in leaderboard and added loading animation

--- frontend/src/components/leaderboard/LeaderList.js
+++ frontend/src/components/leaderboard/LeaderList.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react/jsx-filename-extension */
@@ -8,8 +9,8 @@ function LeaderList(props) {
 
     return (
         <div className="leaderlist">
-            {leaders.map((leader) => (
-                <div className="leaderlist-item">
+            {leaders.map((leader, index) => (
+                <div className="leaderlist-item" key={index}>
                     <img
                         className="avatar-list"
                         src={`https://avatars.dicebear.com/api/human/${leader.username}.svg`}
