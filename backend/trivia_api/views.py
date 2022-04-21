d6f28b6b0e30cda6b18faebaf3515ebcc5761bac
Rushi Patel <rnpnull@gmail.com>
2022-04-21 16:15:20 -0400

    Modified CI instructions to use pylint and correctly locate .pylintrc configuration; linted all files and resolved warnings/errors

--- backend/trivia_api/views.py
+++ backend/trivia_api/views.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
+"""
+Contains all route logic for authentication + data retrieval
+and storage required by the user interface.
+"""
+
+from flask import Blueprint, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
+from sqlalchemy import desc
 from .models import is_category_valid, validate_json
 from .models import User, Result
-from sqlalchemy import desc
 from . import db, login_manager
 
 # Global variables
@@ -30,11 +35,6 @@ dummy_users = {
 }
 
 
-@blueprint.before_app_first_request
-def setup_app():
-    pass
-
-
 @blueprint.route("/")
 def index():
     """
@@ -44,25 +44,14 @@ def index():
     return blueprint.send_static_file("index.html")
 
 
-@blueprint.route("/users", defaults={"user_id": None})
-@blueprint.route("/users/<user_id>")
-def users(user_id):
-    """
-    Dummy user data endpoint.
-    """
-
-    if user_id:
-        result = list(filter(lambda x: x["id"] == int(user_id), dummy_users["users"]))
-
-        if len(result) > 0:
-            return result[0]
-        return {}
-
-    return dummy_users
-
-
 @blueprint.route("/api/signup", methods=["POST"])
 def signup_post():
+    """
+    Handles submission of user authentication data to create
+    a new user if nonexistant and submission is valid. Saves
+    user with hashed password to database.
+    """
+
     data = validate_json(request.data, ["username", "password"])
     if not (data and len(data["username"]) > 0 and len(data["password"]) > 0):
         return {"success": False, "error": "invalid payload"}
@@ -89,6 +78,12 @@ def signup_post():
 
 @blueprint.route("/api/login", methods=["POST"])
 def login_post():
+    """
+    Logs user in using authentication info retrieved
+    from JSON body. Returns success-indicating JSON
+    body for frontend use.
+    """
+
     data = validate_json(request.data, ["username", "password"])
     if not (data and len(data["username"]) > 0 and len(data["password"]) > 0):
         return {"success": False, "error": "invalid payload"}
@@ -108,13 +103,23 @@ def login_post():
 @blueprint.route("/api/logout")
 @login_required
 def logout():
+    """
+    Logs user out and ends their session.
+    """
+
     logout_user()
     return {"success": True}
 
 
 @blueprint.route("/api/me")
 @login_required
-def user_info():
+def user_info_api():
+    """
+    Fetches currently authenticated user's information. Used
+    by the frontend to keep user session state across reloads,
+    and will return an error if no user is logged in.
+    """
+
     user = User.query.get(current_user.id)
 
     user_info = {"username": user.username, "plays": user.plays, "points": user.points}
@@ -132,6 +137,13 @@ def user_info():
 @blueprint.route("/api/quiz", methods=["POST"])
 @login_required
 def submit_quiz():
+    """
+    Submission route for completed quiz. Score is
+    calculated in the frontend and sent to the backend
+    along with information about the quiz (subject,
+    difficulty).
+    """
+
     data = validate_json(request.data, ["category", "score", "maximum"])
     if not (data and is_category_valid(data["category"])):
         return {"success": False, "error": "invalid payload"}
@@ -175,6 +187,14 @@ def submit_quiz():
 @blueprint.route("/api/achievements")
 @login_required
 def get_achievements():
+    """
+    Get the user's achievements. Achievements are
+    provided to the user for hitting certain
+    milestones by play count and points earned
+    on a category-by-category basis. Returned object
+    contains achievements by category in lists.
+    """
+
     achievements = {}
 
     user = User.query.get(current_user.id)
@@ -198,6 +218,13 @@ def get_achievements():
 
 @blueprint.route("/api/leaderboard")
 def get_leaderboard():
+    """
+    Retrieve leaderboard data; accepts optional URL
+    query parameters specifying the category to fetch
+    leaderboard results for. Returns entries with
+    usernames and scores.
+    """
+
     category = request.args.get("category", default="0")
     if category != "0":
         if not is_category_valid(category):
@@ -220,4 +247,9 @@ def get_leaderboard():
 
 @login_manager.unauthorized_handler
 def login_error():
+    """
+    Responds to all authentication-requiring requests
+    with a default body if the user is not logged in.
+    """
+
     return {"success": False, "error": "login required"}
