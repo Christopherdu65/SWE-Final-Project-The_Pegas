f210e16862352d3db5e3d479add74d7d698c1502
Rushi Patel <rnpnull@gmail.com>
2022-04-22 05:21:27 -0400

    Renamed tests folder, removed excess dummy code, added pytest cache to gitignore

--- backend/trivia_api/tests/conftest.py
+++ backend/trivia_api/tests/conftest.py
@@ -1,18 +1,22 @@
-"""conftest.py"""
+"""
+Configuration of mock fixtures.
+"""
 
-import pytest
-from unittest.mock import patch
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
 import os
+from unittest.mock import patch
+import pytest
+from werkzeug.security import generate_password_hash
 from ..models import User, Result
 from .. import create_app
 
 
 @pytest.fixture
 def mock_user():
-    mock_user = User(
+    """
+    Mocked user object fixture.
+    """
+
+    mock_user_object = User(
         id=1,
         username="username",
         password=generate_password_hash("password"),
@@ -20,11 +24,17 @@ def mock_user():
         points={"0": 15, "1": 15},
     )
 
-    return mock_user
+    return mock_user_object
 
 
 @pytest.fixture
 def mock_recent_results(mocker):
+    """
+    Patches the .recents property on the
+    User object to prevent the fetch/set
+    from calling actual database methods
+    """
+
     mock_result = Result(category=1, score=15, maximum=30, user_id=1)
 
     mock = mocker.patch(
@@ -38,6 +48,11 @@ def mock_recent_results(mocker):
 
 @pytest.fixture
 def mock_get_sqlalchemy(mocker):
+    """
+    Patch the SQLAlchemy query object to remove
+    necessity for a live database and allow mocking.
+    """
+
     mock = mocker.patch(
         "flask_sqlalchemy._QueryProperty.__get__"
     ).return_value = mocker.Mock()
@@ -47,19 +62,14 @@ def mock_get_sqlalchemy(mocker):
 
 @pytest.fixture
 def mock_flask_app():
+    """
+    Create a test Flask mock environment with
+    dummy parameters.
+    """
+
     with patch.dict(
         os.environ, {"DATABASE_URL": "postgresql://", "SECRET_KEY": "secret"}
     ):
         mock_app = create_app(test=True)
 
     return mock_app
-
-
-# @pytest.fixture
-# def mock_result_property(mocker):
-#     mock = mocker.patch(
-#         "trivia_api.models.Result.",
-#         new_callable=mocker.PropertyMock,
-#         return_value="new_property_value"
-#     )
-#     return mock
--- backend/trivia_api/tests/test_mocked.py
+++ backend/trivia_api/tests/test_mocked.py
@@ -1,7 +1,16 @@
+"""
+Mocked tests for server-based testing of
+authentication and user state.
+"""
+
 import json
 
 
 def test_login(mock_user, mock_get_sqlalchemy, mock_flask_app):
+    """
+    Use mocked user + Flask app to verify the login process.
+    """
+
     mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_user
 
     with mock_flask_app.test_client() as client:
@@ -14,6 +23,10 @@ def test_login(mock_user, mock_get_sqlalchemy, mock_flask_app):
 
 
 def test_login_incorrect_password(mock_user, mock_get_sqlalchemy, mock_flask_app):
+    """
+    Attempt the login test with incorrect credentials to test security.
+    """
+
     mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_user
 
     with mock_flask_app.test_client() as client:
@@ -25,7 +38,12 @@ def test_login_incorrect_password(mock_user, mock_get_sqlalchemy, mock_flask_app
     assert not response.json["success"]
 
 
+# pylint: disable=unused-argument
 def test_user_info(mock_user, mock_get_sqlalchemy, mock_flask_app, mock_recent_results):
+    """
+    Test the user-state route to ensure well-formed response
+    to be used by frontend.
+    """
     mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_user
     mock_get_sqlalchemy.get.return_value = mock_user
 
